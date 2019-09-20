d7e5be1e1b3b52dab0a3e4143178f296fc07ff34
Qing Ma <qing.ma@isatis-health.nl>
2019-09-20 22:29:27 +0200

    do the same by mocking the child HeroComponent

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { HeroesComponent } from "./heroes.component"
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, Component, Input } from "@angular/core";
 import { HeroService } from "../hero.service";
 import { of } from "rxjs";
+import { Hero } from "../hero";
 
 describe('HeroesComponent (shallow test)', () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES;
+
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>'
+    })
+    class FakeHeroComponent {
+        @Input() hero: Hero;
+        // @Output() delete = new EventEmitter();
+    }
 
     beforeEach(() => {
         HEROES = [
@@ -17,11 +27,14 @@ describe('HeroesComponent (shallow test)', () => {
         ];
         mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [
+                HeroesComponent, 
+                FakeHeroComponent
+            ],
             providers: [
                 { provide: HeroService, useValue: mockHeroService}
             ],
-            schemas: [NO_ERRORS_SCHEMA]
+            // schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
     })
